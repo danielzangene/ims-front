7cf2f4747a1ee74731f0591dc4cff249a2e0fc10
dnaiel zangene <46925439+danielzangene@users.noreply.github.com>
2022-09-08 11:49:30 +0430

    pages

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,24 +1,21 @@
-import {User} from 'react-feather'
 import cartable from "./cartable"
 import dashboard from "./dashboard"
 import team from "./team"
-import setting from "./setting"
-import profile from "./profile"
 import dining from "./dining"
 // import chat from "./chat"
 
 export default [
     ...dashboard,
-    ...profile,
+    // ...profile,
     ...team,
     // ...chat,
     ...cartable,
-    ...dining,
-    ...setting,
-    {
-        id: 'test',
-        title: 'تست',
-        icon: <User size={20}/>,
-        navLink: '/test'
-    }
+    ...dining
+    // ...setting,
+    // {
+    //     id: 'test',
+    //     title: 'تست',
+    //     icon: <User size={20}/>,
+    //     navLink: '/test'
+    // }
 ]
